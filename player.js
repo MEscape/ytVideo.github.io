bdf3497fef16c14751c4887b1f2dea800a56d299
MEscape <123681715+MEscape@users.noreply.github.com>
2024-02-22 13:10:50 +0100

    Update player.js

--- player.js
+++ player.js
@@ -19,28 +19,15 @@ function onYouTubeIframeAPIReady() {
     })
 }
 
-function togglePlayButton(play) {    
-    const stopImg = currentAudio.firstElementChild.firstElementChild
-    play ? stopImg.classList.add('none') : stopImg.classList.remove('none');
-}
-
 function toggleAudio() {
     if (player.getPlayerState() == 1 || player.getPlayerState() == 3 ) {
         player.pauseVideo()
-        togglePlayButton(false)
     } else {
         player.playVideo()
-        togglePlayButton(true)
     } 
 } 
 
 function onPlayerReady() {
     player.setPlaybackQuality("small")
-    togglePlayButton(player.getPlayerState() !== 5)
     toggleAudio()
-}
-
-function onPlayerStateChange(event) {
-    if (event.data === 0) 
-        togglePlayButton(false) 
 }
